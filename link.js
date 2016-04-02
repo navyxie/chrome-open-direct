0172941e949112af84d9010f404fb196102fc9f2
navyxie <navyxie2010@gmail.com>
2016-04-02 12:33:07 +0800

    fix decodeURIComponent bug

--- link.js
+++ link.js
@@ -1,6 +1,5 @@
 ;(function(win,doc){
     doc.addEventListener('click',function(e){
-        console.log(e);
         var target = e.target;
         if(target.tagName === 'A'){
             var href = target.getAttribute('href');
@@ -14,18 +13,19 @@
     function pickUrl(url){
         var originUrl = url;
         try{
-            url = decodeURIComponent(url);
+            
             var paramArr = url.split('&');
             for(var i = 0 , len = paramArr.length ; i < len; i++){
                 var mapObj =  paramArr[i].split('=');
                 if(mapObj[0] === 'url'){
-                    originUrl = mapObj[1];
+                    originUrl = url = decodeURIComponent(mapObj[1]);
                     break;
                 }
             }
         }catch(e){
 
         }
+        console.log(originUrl);
         return originUrl;
     }
 })(window,document);
