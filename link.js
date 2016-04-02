5904db566f0415f2ad04db226eceb677495457ee
navyxie <navyxie2010@gmail.com>
2016-04-02 14:08:04 +0800

    修正正则的bug

--- link.js
+++ link.js
@@ -3,29 +3,25 @@
         var target = e.target;
         if(target.tagName === 'A'){
             var href = target.getAttribute('href');
-            if(href && /[^(http)]/i.test(href)){
-                href = pickUrl(href);
-                e.preventDefault();
-                window.open(href);
+            if(href && /^[^(https:)]/i.test(href)){     
+                target.setAttribute('href',pickUrl(href));
             }
         }
-    },false)
+    },false);
     function pickUrl(url){
         var originUrl = url;
         try{
-            
             var paramArr = url.split('&');
             for(var i = 0 , len = paramArr.length ; i < len; i++){
                 var mapObj =  paramArr[i].split('=');
                 if(mapObj[0] === 'url'){
-                    originUrl = url = decodeURIComponent(mapObj[1]);
+                    originUrl = decodeURIComponent(mapObj[1]);
                     break;
                 }
             }
         }catch(e){
 
         }
-        console.log(originUrl);
         return originUrl;
     }
 })(window,document);
